3735777c61cda7fbc9e1351139d37491d73aca10
NeelPatel5150 <neelp2106@gmail.com>
2026-05-31 14:30:16 +0530

    Add lexical scope explanation and implement function to find highest marks in student array

--- Day 8 Javascript - Phase 3/script.js
+++ Day 8 Javascript - Phase 3/script.js
@@ -16,3 +16,31 @@
 // -! when a function is called, it is added to the call stack. The call stack is a data structure that keeps track of the execution context of functions. When a function finishes executing, it is removed from the call stack, and control returns to the previous function in the stack. This process allows JS to manage function calls and execute code in an organized manner.
 
 //! lexical scope : variable apne parent function ke andar access kar sakta hai but parent function apne child function ke variable ko access nahi kar sakta hai. This is because of the way JS handles scope and variable resolution. When a variable is accessed, JS looks for it in the current scope first, then in the parent scope, and so on until it reaches the global scope. If the variable is not found in any of these scopes, it will result in a ReferenceError. This is why child functions can access variables from their parent functions, but parent functions cannot access variables from their child functions.
+
+// Find Highest Marks with name show in array of Objects
+var students = [
+    {
+        name: "Neel",
+        marks:90
+    },
+    {
+        name: "Dev",
+        marks:100
+    },
+    {
+        name: "Mahi",
+        marks:50
+    }
+]
+
+let highest = students[0];
+
+for(let i=1;i<students.length;i++)
+{
+    if(students[i].marks > highest.marks)
+    {
+        highest = students[i]
+    }
+}
+
+console.log("Highest :",highest.name);
