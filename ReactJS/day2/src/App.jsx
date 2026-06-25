ea79469be3ff8438b249ba3259f16f0c48ac94e7
NeelPatel5150 <neelp2106@gmail.com>
2026-06-25 22:24:32 +0530

    Add initial setup for React project with Vite
    - Created main entry point (main.jsx) for the React application.
    - Added global styles (index.css) for the application.
    - Included React and Vite configuration files (vite.config.js).
    - Added SVG assets for React and Vite logos (react.svg, vite.svg).

---
+++ ReactJS/day2/src/App.jsx
@@ -0,0 +1,20 @@
+import  {useState} from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  function increment()
+  {
+    setCount(count + 1);
+  }
+  
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+
+      <button onClick={increment}>Increment</button>
+    </div>
+  )
+}
+
+export default App
---
+++ ReactJS/day2/src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div style={{ backgroundColor: 'yellow', padding: '1rem' }}>
+      This is the navbar
+    </div>
+  )
+}
+
+export default Navbar
