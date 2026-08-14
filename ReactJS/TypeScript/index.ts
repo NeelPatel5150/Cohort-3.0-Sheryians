772e39c883f3ed6a27d1bf842730ab50653c50c2
NeelPatel5150 <neelp2106@gmail.com>
2026-08-14 20:35:47 +0530

    Typescript Done

---
+++ ReactJS/TypeScript/index.ts
@@ -0,0 +1,87 @@
+// - Type Inference and Annotation
+// let a: string = "hello";
+// a = "pol";
+// a = "bye";
+// console.log(a);
+
+// primitive data types
+
+// string,
+// let val: string = "Shubham";
+
+// boolean,
+// let val2: boolean = true;
+
+// number,
+// let b: number = 90;
+
+// undefined,
+// let h: undefined = undefined;
+
+// bigint,
+// let g: bigint = 79697678n;
+
+// symbol
+// let y: symbol = Symbol("hello");
+
+// console.log("arrays tuples.");
+
+// let a: unknown = "faizal";
+
+// console.log(a.toUpperCase());
+
+// let y: never;
+
+// - Arrays
+let arr: number[] = [
+  5, 6, 7, 8, 9, 0, 67, 8, 9, 0, 6, 5, 43, 2, 3, 4, 5, 6, 7, 8, 9, 0, 87, 6, 5,
+  4, 3,
+];
+
+let arr1: string[] = ["jio", "pol", "john"];
+
+let arrBol: boolean[] = [true, false, true, true, true];
+
+// - Tuples
+
+let arr2: [number, number, string, boolean] = [56, 78, "pol", true];
+
+let data: any[] = [{ name: "pol" }, { name: "pol" }, { name: "pol" }];
+
+// enums - options
+enum Role {
+  ADMIN,
+  SUP_ADMIN,
+  USER,
+}
+let role: Role = Role.USER;
+
+// union
+let yolo: string | number = "rahul";
+yolo = 90;
+
+// literals
+type Status = "pending" | "success" | "error";
+
+let status: Status = "pending";
+
+// Objects
+
+let userObj = {
+  name: "pol",
+  age: 23,
+  isAdmin: true,
+  company: "poltech",
+  address: {
+    street: "ahmedabad",
+    city: "gujarat",
+    state: "GJ",
+  },
+};
+
+let sum = (a:number, b:number) => {
+  return a + b;
+}
+
+console.log(sum(6, 6));
+
